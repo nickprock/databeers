91961e390b8046eecbd855b78e8ed95f4523bb08
Nico <nico.pro412@gmail.com>
2018-12-05 12:50:04 +0100

    create MAIN.R

---
+++ script/MAIN.R
@@ -0,0 +1,12 @@
+# carica il dataset
+source(file = "script/functions.R")
+data <- loadBeerReviews()
+# cerca la birra
+getBeerList(letter = "Bush De No")
+# recEngine
+source(file = "script/compareMyBeer.R")
+calc_similarity(beer1 = "Bush De Noël", 
+                beer2 = "Bush De Noël Premium")
+calc_similarity(beer1 = "Fat Tire Amber Ale", 
+                beer2 = "Dale's Pale Ale")
+recommended_Beers <- compareMyBeer(myBeer = "Corona Extra")
